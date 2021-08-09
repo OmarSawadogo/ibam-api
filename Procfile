5e1882d2d1803321a5db2046102123d7878506c8
somar <somar@ticanalyse.org>
2021-08-09 18:22:55 +0000

    2

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-release: python manage.py makemigrations --no-input
-release: python manage.py migrate --no-input
-
 web: gunicorn api.wsgi
+release: python manage.py makemigrations --noinput
+release: python manage.py collectstatic --noinput
+release: python manage.py migrate --noinput
--- api/settings.py
+++ api/settings.py
@@ -23,14 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g1i0jir+mvpqi_&)m7nhpp+8y-4348+#mx-!^wkm*c1l3ki7+k'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ibam-api.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
